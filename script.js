3f30fdd7c784ea480b4878aaa21fb00975f51ecb
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-02-06 12:33:49 -0500

    arehmanlatif1

---
+++ script.js
@@ -0,0 +1,23 @@
+const URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+const promise = fetch(URL);
+console.log(promise);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.getElementById('form');
+// const getResult = function(event){
+    
+// }
