58e18ed1351751a32cce12fe148561a20c4278bc
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-02-11 02:54:26 -0500

    abdul

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-const URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
-const promise = fetch(URL);
-console.log(promise);
+const baseUrl = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/pkr.json";
+const dropdowns = document.querySelectorAll(".dropdown");
 
 
 
 
 
+// function getTemperature() {
+//     const latitude = document.getElementById("inputLatitude").value;
+//     const longitude = document.getElementById("inputLongitude").value;
+//     const unit = document.getElementById("unitSelect").value;
+
+//     const baseUrl = "https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m";
+
+//     fetch(baseUrl)
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error("Network response was not OK");
+//         }
+//          return response.json();
+//     })
+
+//     .then(data => {
+//         const temperature = data.hourly.temperature_2m.value;
+//         const unitySymbol = unit === 'metric' ? '°C' : '°F';
+//         const resultDiv = document.getElementById("result");
+//         resultDiv.innerHTML = '<p>Current temperature: ${temperature} ${unitSymbol} <p>'
+//     })
 
 
-
-
-
-
-
-
-
-
-
-// const form = document.getElementById('form');
-// const getResult = function(event){
-    
 // }
+ 
+
+
+
+
+
+
+
