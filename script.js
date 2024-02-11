ace38d97878f898bd1ebb3bfbc66c12acfc7b3f2
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-02-11 18:29:07 -0500

    ar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const baseUrl =  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector("to select");
+const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
 
